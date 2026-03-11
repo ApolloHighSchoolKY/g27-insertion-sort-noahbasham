3fd0bd88d82045b278e6eda94a187d5c3731975b
noahbasham <noah.basham@stu.daviess.kyschools.us>
2026-03-11 01:35:41 +0000

    no clue on the arraylist part, did my best

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,9 +1,39 @@
+import java.util.Arrays;
+
 public class AInsertionSort
 {
 
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
+        System.out.println(Arrays.toString(myNumbers));
+
+        boolean switchedIt;
+
+        for(int currentPosition = 1; currentPosition < myNumbers.length; currentPosition++)
+        {
+            int currentNum = myNumbers[currentPosition];
+            switchedIt = false;
+            for(int currentComparison = 0; currentComparison < currentPosition; currentComparison++)
+            {
+                int currentCompNum = myNumbers[currentComparison];
+                if(currentNum < currentCompNum && !switchedIt)
+                {
+                    
+                    for(int currentMove = currentPosition; currentMove > currentComparison; currentMove--)
+                    {
+                        int previousNum = myNumbers[currentMove - 1];
+                        myNumbers[currentMove] = previousNum;
+                    }
+                    
+                    myNumbers[currentComparison] = currentNum;
+                    
+                    switchedIt=true;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(myNumbers));
 
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.Arrays;
 public class ALInsertionSort
 {
 
@@ -10,25 +10,45 @@ public class ALInsertionSort
     	boolean inserted=false;
 
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
-
+		for(int currentPosition = 1; currentPosition<myNumbers.length; currentPosition++){
 			//Check with each of the numbers in the sorted list
+			inserted=false;
+			for(int currentComparison = 0; currentComparison<currentPosition; currentComparison++)
+			{
 
 				//If this number is less than one in the sorted list,
 				//insert it there
-
+				if(myNumbers[currentPosition] < sorted.get(currentComparison) && !inserted)
+				{
+					sorted.add(currentComparison, myNumbers[currentPosition]);
+					inserted = true;
+				}
 			//If it was not inserted, stick it on the end.
-
-
+				if(!inserted)
+					sorted.add(myNumbers[currentPosition]);
+				System.out.println(sorted);
+			}
 		//End Loop for unsorted list
+		}
 
 		System.out.println(sorted);
 
 		//Move the data back to the array
-
+		int currentVal = 0;
+		int position = 0;
+		for(int i = 0; i < sorted.size(); i++)
+		{
+			if(sorted.get(i)>=currentVal && position < myNumbers.length)
+			{
+				myNumbers[position] = sorted.get(i);
+				position++;
+				currentVal = sorted.get(i);
+			}
+		}
 		//Print the contents of the array
-
+		System.out.println(Arrays.toString(myNumbers));
     }
 
 
